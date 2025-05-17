a37dbc752a182fc845c63da6d700f052b5705181
Brian-E-Ramirez-Zea <brian.ramirez6@upr.edu>
2025-05-17 13:32:56 -0400

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://Brian-E-Ramirez-Zea.github.io',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
