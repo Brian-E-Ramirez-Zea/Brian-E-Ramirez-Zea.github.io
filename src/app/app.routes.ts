8473513d2acaf2e1a5677414f4e79ba741122533
Brian-E-Ramirez-Zea <brian.ramirez6@upr.edu>
2025-05-18 19:01:23 -0400

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-// src/app/app.routes.ts
-import { Routes } from '@angular/router';
-import { ExperienceComponent } from './experience/experience.component';
-
-export const routes: Routes = [
-  { path: 'experience', component: ExperienceComponent },
-  { path: '', redirectTo: '/experience', pathMatch: 'full' }
-];
